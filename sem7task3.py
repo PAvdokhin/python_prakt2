b8259867735c6cb6550f7c97089a4d860082c9a3
PAvdokhin <p.avdokhin@g.nsu.ru>
2023-03-18 11:27:20 +0700

    sem7

--- sem7task3.py
+++ sem7task3.py
@@ -1,6 +1,24 @@
 import unittest
 import os
+
+
 #ОСТАЛЬНЫЕ 2 ЗАДАНИЯ В ВЕТКЕ sem5
 
-    def CreateText():
-        os.makedirs(r'C:\Users\Acer\Desktop\taskpythyo\mr_chogich')
+
+a = r'C:\Users\Acer\Desktop\taskpythyo\mr_chogich'
+
+
+def CreateText(a):
+    os.makedirs(a)
+    with open(r'C:\Users\Acer\Desktop\taskpythyo\mr_chogich\text.txt', "w") as text:
+        text.write('The cake is a lie')
+
+class TestSetUpText(unittest.TestCase):
+    def setUp(self):
+        self.create = CreateText(a)
+
+    def test_assert_is_not_none(self):
+        self.assertIsNotNone(r'C:\Users\Acer\Desktop\taskpythyo\mr_chogich\text.txt')
+    def tearDown(self):
+        os.remove(r'C:\Users\Acer\Desktop\taskpythyo\mr_chogich\text.txt')
+        os.rmdir(r'C:\Users\Acer\Desktop\taskpythyo\mr_chogich')
