6e45a5a1b9def4b3fbc34c856a9b48b9069b666b
PAvdokhin <p.avdokhin@g.nsu.ru>
2023-06-04 20:26:43 +0700

    Семинар 6, Задания

---
+++ sem6task2.py
@@ -0,0 +1,22 @@
+import time
+
+st = time.time()
+
+
+def number_is_even(n):
+    return n % 2 == 0
+
+
+a = number_is_even(310284)
+b = number_is_even(2104)
+c = number_is_even(102001)
+d = number_is_even(10029481)
+end = time.time()
+delta = st - end
+print(delta)
+
+
+file = f"sem6task2(1){time.strftime('%d.%m.%Y')}.txt"
+with open (file, 'w') as f:
+    f.write(f"Время выполнения операции: {delta} секунд")
+
--- sem7task3.py
+++ sem7task3.py
@@ -23,5 +23,5 @@ class TestSetUpText(unittest.TestCase):
         os.remove(r'C:\Users\Acer\Desktop\taskpythyo\mr_chogich\text.txt')
         os.rmdir(r'C:\Users\Acer\Desktop\taskpythyo\mr_chogich')
 
-if __name__ == 'main'
+if __name__ == 'main':
     unittest.main()
