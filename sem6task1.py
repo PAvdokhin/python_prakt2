30a921322d6a7fc7ea7cb879bb8961b97cb90a04
PAvdokhin <p.avdokhin@g.nsu.ru>
2023-06-29 21:46:20 +0700

    Семинар 7 Задания

--- sem6task1.py
+++ sem6task1.py
@@ -1,7 +1,7 @@
 import time
 a = int(input('Введите число - '))
-while a > 0:
+if a > 0:
     time.sleep(a)
     a = int(input('Введите число - '))
-while a < 0:
-    break
+else:
+    print("Непредвиденная ошибка")
